e1c54fd872f2d926a22a11569b8ec329a7248a6c
Luke Charman <hello@lukecharman.com>
2025-05-08 16:29:22 +0100

    More tweaks.

--- TODO.swift
+++ TODO.swift
@@ -6,7 +6,11 @@
  * Reuse that in Shortcuts / Siri / Control Center.
  * More Analytics events.
  * Clean up watch's UI and widgets.
+ * Clean up Morsel vs. keyboard height once and for all.
+    * Maybe he disappears, keyboard opens, then he slides down from the top with a little rope?
  * Add more expression to Morsel.
+    * Gradual smile / sad while you're deciding who a morsel is for.
+ * Clean up the DestinationPicker's UI a little, fonts, etc.
  * Decide on final set of options in Stats / Extras. Color for Morsel, with accompanying app icons, etc.?
 
  SHOULD
@@ -16,5 +20,6 @@
  COULD
  -----
  * Add some reminder notifications, maybe with a reminder time? User can select up to 3 times per day?
+ * Togglable widget background showing red to green for food for you versus Morsel that day?
 
  */
