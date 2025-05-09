2286df9592cdd2247f3e026b31a3a19006444f56
Luke Charman <hello@lukecharman.com>
2025-05-09 14:35:50 +0100

    Intents, not fully working.

--- Core/FeedMorselIntent.swift
+++ Core/FeedMorselIntent.swift
@@ -1,17 +1,18 @@
 import AppIntents
-import SwiftData
 
 struct FeedMorselIntent: AppIntent {
   static var title: LocalizedStringResource = "Feed Morsel"
   static var description = IntentDescription("Resist a craving and feed it to Morsel.")
+  static var parameterSummary: some ParameterSummary {
+    Summary("Feed \(\.$item) to \(.applicationName)")
+  }
   static var openAppWhenRun: Bool = false
 
-  @Parameter(title: "Craving")
+  @Parameter(title: "Item")
   var item: String
 
   func perform() async throws -> some IntentResult {
-    
-    print("Fed to Morsel: \(item)")
-    return .result(dialog: "Fed \(item) to Morsel.")
+    try await Adder.add(name: item, isForMorsel: true, context: .phoneIntent)
+    return .result(dialog: "Fed \(item) to Morsel!")
   }
 }
