584313903d5396567f84c84e6b17aa2d9d44a37a
Luke Charman <hello@lukecharman.com>
2025-05-10 20:10:31 +0100

    Working Intents.

--- Core/Intents/MorselShortcuts.swift
+++ Core/Intents/MorselShortcuts.swift
@@ -11,6 +11,14 @@ struct MorselShortcuts: AppShortcutsProvider {
       shortTitle: "Feed Morsel",
       systemImageName: "fork.knife"
     )
+    AppShortcut(
+      intent: FeedMeIntent(),
+      phrases: [
+        "Log food in \(.applicationName)"
+      ],
+      shortTitle: "Feed Me",
+      systemImageName: "fork.knife"
+    )
   }
   static var shortcutTileColor: ShortcutTileColor = .grayBlue
 }
