81e42d3a9025b28b7873213f48636adb6d2d1dac
Luke Charman <hello@lukecharman.com>
2025-05-13 20:22:06 +0100

    Add a custom Text Field to handle hitting Done while the field is empty. Thanks SwiftUI.

---
+++ iOS/Views/Helpers/MorselTextField.swift
@@ -0,0 +1,54 @@
+import Foundation
+import SwiftUI
+import UIKit
+
+struct MorselTextField: UIViewRepresentable {
+  @Binding var text: String
+  @Binding var isFocused: Bool
+  var onValidSubmit: () -> Void
+  var onInvalidSubmit: () -> Void
+
+  class Coordinator: NSObject, UITextFieldDelegate {
+    var parent: MorselTextField
+
+    init(_ parent: MorselTextField) {
+      self.parent = parent
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      if parent.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+        parent.onInvalidSubmit()
+        return false
+      }
+      parent.onValidSubmit()
+      return true
+    }
+
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+      parent.text = textField.text ?? ""
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+
+  func makeUIView(context: Context) -> UITextField {
+    let tf = UITextField()
+    tf.delegate = context.coordinator
+    tf.textAlignment = .center
+    tf.font = UIFont(name: "Quicksand-Semibold", size: 16)
+    tf.returnKeyType = .done
+    tf.addTarget(context.coordinator, action: #selector(Coordinator.textFieldDidChangeSelection(_:)), for: .editingChanged)
+    return tf
+  }
+
+  func updateUIView(_ uiView: UITextField, context: Context) {
+    uiView.text = text
+    if isFocused && !uiView.isFirstResponder {
+      uiView.becomeFirstResponder()
+    } else if !isFocused && uiView.isFirstResponder {
+      uiView.resignFirstResponder()
+    }
+  }
+}
