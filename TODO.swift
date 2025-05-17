f8d3a1aa70ef06f6d6239384b596cd9e02e5fb22
Luke Charman <hello@lukecharman.com>
2025-05-17 09:41:51 +0100

    Lots of notes and organisation.

--- TODO.swift
+++ TODO.swift
@@ -4,22 +4,50 @@
  ----
  * Add more expression to Morsel.
     * Gradual smile / sad while you're deciding who a morsel is for.
+    * Expression for “conflicted” or “uncertain”
+    * Perfect the idle loop
+    * Then STOP. That's 1.0.
+
  * Decide on final set of options in Stats / Extras. Color for Morsel, with accompanying app icons, etc.?
-    * Toggle for Apple / custom fonts?
-    * Always dark / light mode?
+    * Colour picker (simple palette of 6 colors, not free-form)
+    * App icon changer tied to that colour picker
+
+ * Weekly "Digest"
+
  * Add an Onboarding of some kind.
- * Decide on subscription vs. one-off. £1.99 a year? £0.99 a year?
- * Implement IAPs.
+    * One screen, super simple, ON the empty state.
+    * Animated, beautiful. Can Morsel wave?
+
+ * Decide on £0.99 a year subscription.
+    * Fallbacks for missing entitlements / failed purchases
+    * Basic crash/error logging (e.g. Firebase or similar)
+
  * More Analytics events.
+    * Ensure all screen views are done.
+    * log_craving
+    * delete_craving
+    * log_meal
+    * delete_meal
+    * expression_shown (on Morsel)
+    * change_setting
+    * started_onboarding
+    * completed_onboarding
+    * tapped_subscribe
+    * completed_purchase
+    * received_weekly_digest
+    * opened_weekly_digest
 
  SHOULD
  ------
  * More haptics / sound effects (with a toggle in Extras).
- * Figure out this custom AppEntity thing for speaking the item in one term to Siri.
+    * Tamagotchi gulp when feeding
 
  COULD
  -----
- * Add some reminder notifications, maybe with a reminder time? User can select up to 3 times per day?
+ * Figure out this custom AppEntity thing for speaking the item in one term to Siri.
+ * Add some reminder notifications, NOT NAGS. Maybe with a reminder time? User can select up to 3 times per day?
  * Togglable widget background showing red to green for food for you versus Morsel that day?
+ * Daily goal streaks?
+ * “Random tip from Morsel” in Extras. Could be serious (“Eat slowly”) or playful (“Ice cream is legal even on Tuesdays”)
 
  */
