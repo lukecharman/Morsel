3a482cd1be714bb9e829b84a2991cccf351b4464
Luke Charman <hello@lukecharman.com>
2025-06-02 10:22:54 +0100

    Move a few more bits about.

--- Core/Analytics/Analytics.swift
+++ Core/Analytics/Analytics.swift
@@ -4,8 +4,8 @@ import TelemetryDeck
 struct Analytics {
   static func setUp() {
     let config = TelemetryDeck.Config(appID: "0450C03B-3699-46A4-A99C-FB9F78C887E2")
-    config.defaultSignalPrefix = "Morsel."
-    config.defaultParameterPrefix = "Morsel."
+    config.defaultSignalPrefix = "morsel."
+    config.defaultParameterPrefix = "morsel."
 
     TelemetryDeck.initialize(config: config)
   }
