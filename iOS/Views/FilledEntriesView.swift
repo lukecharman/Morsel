eff60361959f3cb20244dd00bb0acef639305c89
Luke Charman <hello@lukecharman.com>
2025-09-03 20:30:36 +0100

    Softer edges.

--- iOS/Views/FilledEntriesView.swift
+++ iOS/Views/FilledEntriesView.swift
@@ -86,18 +86,19 @@ struct FilledEntriesView: View {
     )
   }
 
-  private var mask: LinearGradient {
+  private var mask: some View {
     LinearGradient(
       gradient: Gradient(stops: [
-        .init(color: .clear, location: 0),
+        .init(color: .clear, location: 0.0),
         .init(color: .black, location: 0.03),
         .init(color: .black, location: 0.92),
         .init(color: .clear, location: 0.95),
-        .init(color: .clear, location: 1)
+        .init(color: .clear, location: 1.0)
       ]),
       startPoint: .top,
       endPoint: .bottom
     )
+    .blur(radius: 22)
   }
 
   private var groupedEntries: [(date: Date, entries: [FoodEntry])] {
